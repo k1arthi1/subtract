a09b98337388c6c6a4830236ea508b21184d164c
k1arthi1 <155720361+k1arthi1@users.noreply.github.com>
2025-01-27 15:15:01 +0530

    Update sub.java

--- sub.java
+++ sub.java
@@ -1,1 +1,11 @@
-sa
+class sub
+  {
+    public static void main (String aregs[])
+    {
+      int a,b,sub;
+      a=9;
+      b=4;
+      sub=a-b;
+      System.out.println("- of two no is "=sub);
+    }
+  }
